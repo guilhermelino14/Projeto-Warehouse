df7cdea6f0be91b717d2ad4b9729e799e35ee071
Leandro Filipe VIeira <2200689@my.ipleiria.pt>
2023-05-03 19:09:50 +0100

    Fix goal_cells_adjacent

--- warehouse/warehouse_problemforSearch.py
+++ warehouse/warehouse_problemforSearch.py
@@ -20,9 +20,9 @@ class WarehouseProblemSearch(Problem[WarehouseState]):
                 self.goal_cells_adjacent.append(Cell(self.goal_position.line + 1, self.goal_position.column))
             if (self.goal_position.column + 1) <= 4 and initial_state.matrix[self.goal_position.line][self.goal_position.column + 1] == constants.EMPTY:
                 self.goal_cells_adjacent.append(Cell(self.goal_position.line, self.goal_position.column  + 1))
-            if (self.goal_position.line - 1) <= 4 and initial_state.matrix[self.goal_position.line - 1][self.goal_position.column] == constants.EMPTY:
+            if (self.goal_position.line - 1) >= 0 and initial_state.matrix[self.goal_position.line - 1][self.goal_position.column] == constants.EMPTY:
                 self.goal_cells_adjacent.append(Cell(self.goal_position.line - 1, self.goal_position.column))
-            if (self.goal_position.column + 1) <= 4 and initial_state.matrix[self.goal_position.line][self.goal_position.column -1] == constants.EMPTY:
+            if (self.goal_position.column - 1) >= 0 and initial_state.matrix[self.goal_position.line][self.goal_position.column -1] == constants.EMPTY:
                 self.goal_cells_adjacent.append(Cell(self.goal_position.line, self.goal_position.column - 1))
 
     def get_actions(self, state: WarehouseState) -> list:
