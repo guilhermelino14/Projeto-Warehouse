e4f0f42f5a3ce9b4e1599ba9df7e2aa5467cc0d8
Leandro Filipe VIeira <2200689@my.ipleiria.pt>
2023-05-25 01:42:27 +0100

    Add __eq__ to Pair

--- warehouse/pair.py
+++ warehouse/pair.py
@@ -10,6 +10,10 @@ class Pair:
         return str(self.cell1.line) + "_" + str(self.cell1.column) + "_" + str(
             self.cell2.line) + "_" + str(self.cell2.column)
 
+    def __eq__(self, o: object) -> bool:
+        return  (self.cell1 == o.cell1 and self.cell2 == o.cell2) or (self.cell1 == o.cell2 and self.cell2 == o.cell1)
+
+
     def __str__(self):
         return str(self.cell1.line) + "-" + str(self.cell1.column) + " / " + str(self.cell2.line) + "-" + str(self.cell2.column) + ": " + str(self.value) + "\n"
 
