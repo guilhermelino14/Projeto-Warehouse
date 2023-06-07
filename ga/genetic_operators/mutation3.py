413fb62bc68d06003f13368c7b6df98500ed693f
Guilherme Lino <lino000000@hotmail.com>
2023-06-07 09:12:35 +0100

    Create mutation3

--- ga/genetic_operators/mutation3.py
+++ ga/genetic_operators/mutation3.py
@@ -1,3 +1,4 @@
+from ga.genetic_algorithm import GeneticAlgorithm
 from ga.individual_int_vector import IntVectorIndividual
 from ga.genetic_operators.mutation import Mutation
 
@@ -6,7 +7,16 @@ class Mutation3(Mutation):
         super().__init__(probability)
 
     def mutate(self, ind: IntVectorIndividual) -> None:
-        # TODO
+        for i in range(len(ind.genome)):
+            # Verifica se a mutação ocorre com base na probabilidade
+            if GeneticAlgorithm.rand.random() < self.probability:
+                # selecionar aleatoriamente dois índices diferentes no genoma do indivíduo
+                index1 = GeneticAlgorithm.rand.randint(0, len(ind.genome) - 1)
+                index2 = GeneticAlgorithm.rand.randint(0, len(ind.genome) - 1)
+                while index2 == index1:
+                    index2 = GeneticAlgorithm.rand.randint(0, len(ind.genome) - 1)
+                # Troque os valores nos índices selecionados
+                ind.genome[index1], ind.genome[index2] = ind.genome[index2], ind.genome[index1]
         pass
 
     def __str__(self):
